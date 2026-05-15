bd77a6014c933f9b1d6c411fdf70fcb4bb6e8ba8
dagabear <darigash23@gmail.com>
2026-05-15 22:22:07 +0500

    feat: add exception

---
+++ src/main/java/kz/genvibe/media_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package kz.genvibe.media_management.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(exception = EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleEntityNotFoundException(EntityNotFoundException e, Model model) {
+        log.error("Entity not found: {}", e.getMessage());
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/404";
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleUserAlreadyExistsException(UserAlreadyExistsException e, Model model) {
+        log.error("User already exists: {}", e.getMessage());
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/409";
+    }
+
+    @ExceptionHandler(VerificationLinkExpiredException.class)
+    @ResponseStatus(HttpStatus.GONE)
+    public String handleVerificationLinkExpiredException(VerificationLinkExpiredException e, Model model) {
+        log.error(e.getMessage());
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/410";
+    }
+
+}
